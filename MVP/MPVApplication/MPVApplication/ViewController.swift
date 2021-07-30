d8dcd6f238707ffb89258de20d3b3855a44aeba7
europeec <l.europeec@ya.ru>
2021-07-30 12:50:47 +0300

    add linter && configure & refactoring

---
+++ MVP/MPVApplication/MPVApplication/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MPVApplication
+//
+//  Created by Дмитрий Юдин on 30.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
