fb8e90ed846bc1cff860ba0ab92fb4536e8cb499
europeec <l.europeec@ya.ru>
2021-08-02 20:30:27 +0300

    create folder + yml file for swiftlint

---
+++ MVVMApplication/MVVMApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMApplication
+//
+//  Created by Дмитрий Юдин on 02.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
